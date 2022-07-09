32a0aedf6be6e62fe0490a90abf18a1e345447b7
Kildall <pebustos.135@gmail.com>
2022-07-09 14:39:47 -0300

    Fixed the page boss

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let amount = parseFloat(document.getElementById("amount").value);
+    let interest = parseFloat(document.getElementById("rate").value);
+    let years = parseInt(document.getElementById("years").value);
+
+    let resultAmount = amount * Math.pow(1+(interest/100), years);
+    let yearReceived = new Date().getFullYear() + years;
     
+    document.getElementById("results").style.display = "block";
+    document.getElementById("deposit").textContent = amount;
+    document.getElementById("interest").textContent = interest;
+    document.getElementById("amountReceived").textContent = parseInt(resultAmount);
+    document.getElementById("year").textContent = yearReceived;
+    debugger;
+}
+
+function changeValue(){
+    document.getElementById("rateValue").textContent = document.getElementById("rate").value.toString();
 }
         
