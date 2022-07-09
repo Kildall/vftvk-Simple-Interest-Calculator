2c912f0ffbdfdddf285dfa5dbec055090548f4e0
Kildall <pebustos.135@gmail.com>
2022-07-09 14:54:48 -0300

    Added positive number alert, title, and fixed interest calculation.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function compute()
 {
     let amount = parseFloat(document.getElementById("amount").value);
-    let interest = parseFloat(document.getElementById("rate").value);
+    if(Number.isNaN(amount) || amount < 1){
+        alert('Enter a positive number!');
+        document.getElementById("amount").focus();
+        return;
+    }
+    let interest = parseInt(document.getElementById("rate").value);
     let years = parseInt(document.getElementById("years").value);
 
-    let resultAmount = amount * Math.pow(1+(interest/100), years);
+    let interestAmount = amount * (interest/100) * years;
     let yearReceived = new Date().getFullYear() + years;
     
     document.getElementById("results").style.display = "block";
     document.getElementById("deposit").textContent = amount;
     document.getElementById("interest").textContent = interest;
-    document.getElementById("amountReceived").textContent = parseInt(resultAmount);
+    document.getElementById("amountReceived").textContent = amount + interestAmount;
     document.getElementById("year").textContent = yearReceived;
-    debugger;
 }
 
 function changeValue(){
